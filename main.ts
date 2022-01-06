4685c635519afca4e93af0ece8518d2742cf2c67
Dong-Chen-1031 <90507781+Dong-Chen-1031@users.noreply.github.com>
2022-01-06 16:44:56 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,15 @@ huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
 basic.forever(function () {
     huskylens.request()
-    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-    	
-    } else if (false) {
-    	
+    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        for (let index = 0; index <= 5; index++) {
+            if (huskylens.isAppear(index + 1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+                if (huskylens.isLearned(index + 1)) {
+                	
+                } else {
+                	
+                }
+            }
+        }
     }
 })
