fc9c92aacb7a10c730f638cb43772a3bf418cdf7
Dong-Chen-1031 <90507781+Dong-Chen-1031@users.noreply.github.com>
2022-01-06 16:47:38 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,10 @@ huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
 basic.forever(function () {
     huskylens.request()
     if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        for (let index = 0; index <= 5; index++) {
-            if (huskylens.isAppear(index + 1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-                if (huskylens.isLearned(index + 1)) {
-                	
-                } else {
-                	
-                }
-            }
+        if (huskylens.isLearned(huskylens.readArrow_s(Content4.ID))) {
+            basic.showString("Hello!")
+        } else {
+            basic.showString("Get out")
         }
     }
 })
