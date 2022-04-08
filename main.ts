731ff9db81379a73e9d22d9063626956eab04636
Dong-Chen-1031 <90507781+Dong-Chen-1031@users.noreply.github.com>
2022-04-08 15:34:19 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+radio.setGroup(4)
 basic.forever(function () {
     huskylens.request()
     if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         if (huskylens.readBox_s(Content3.ID) == 0) {
             basic.showIcon(IconNames.No)
+            radio.sendValue("false", huskylens.readBox_s(Content3.ID))
         } else {
             basic.showIcon(IconNames.Yes)
+            radio.sendValue("ture", huskylens.readBox_s(Content3.ID))
         }
     } else {
         basic.clearScreen()
+        radio.sendValue("none", 0)
     }
 })
